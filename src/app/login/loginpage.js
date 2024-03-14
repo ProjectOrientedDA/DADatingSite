d1d6a281584ab3d60307694aea2c45babbc15d95
Franklin Wang <franklinywang@gmail.com>
2024-03-14 16:30:49 -0700

    changed basic template website into a basic login page

---
+++ src/app/login/loginpage.js
@@ -0,0 +1,16 @@
+// LoginPage.js
+import React from 'react';
+
+function LoginPage() {
+  return (
+    <div>
+      {
+        
+      }
+      <h1>Login Page</h1>
+      <p>This is your login page content.</p>
+    </div>
+  );
+}
+
+export default LoginPage;
